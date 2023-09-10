0e52a1befa0fde0107e01cb806296543678b9f0d
Alexandre Osvaldo <alexandreosvaldo@hotmail.com>
2023-09-10 09:58:14 -0300

    Transcrevendo o livro Programação 2

---
+++ C++/basico.c
@@ -0,0 +1,23 @@
+/* Comentários */
+
+/* Estrutura básica*/
+
+# include <stdio.h>
+
+int main(){
+    printf("Aqui é qualquer mensagem que será exibida na tela."); /*todo comando deve terminar em ponto-e-virgula*/
+    return 0; /* no final do comando sempre para retornar 0 (zero) se não houver erro */
+}
+
+/* Em C o programa funciona no seguinte fluxo:
+
+CÓDIGO FONTE --> COMPILADOR --> PROGRAMA OBJETO
+                                        |
+                                        v
+                 BIBLIOTÉCAS ------> LIGADOR ---> EXECUTÁVEL
+
+*/
+
+/* TIPOS DE DADOS */
+/* Identificadores válidos = A, a, media, altura2, media_idade, x35 */
+
